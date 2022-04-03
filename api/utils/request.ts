33ea8793aec4107bc1f9cbd2b730936bcc0a40c0
yuyinws <lyc657508366@gmail.com>
2022-04-03 20:48:11 +0800

    chore:accept language

--- api/utils/request.ts
+++ api/utils/request.ts
@@ -10,6 +10,9 @@ const agent = tunnel.httpsOverHttp({
 })
 const instance = axios.create({
   httpsAgent: process.env.MODE === 'development' ? agent : false,
+  headers: {
+    'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+  },
 })
 
 const request = async(config: AxiosRequestConfig): Promise<any> => {
