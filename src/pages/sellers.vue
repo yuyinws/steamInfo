901f2fe0f7cde13781b7a63fb2b791883437dd56
yuyinws <lyc657508366@gmail.com>
2022-04-08 09:26:30 +0800

    text width

--- src/pages/sellers.vue
+++ src/pages/sellers.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="sellers.length && sellers[0]._base64" mb-100px pb-12 pt-12 p-2>
     <div flex="~ row gap-4 wrap" justify-center>
-      <div v-for="(item, index) in sellers" :key="index" @click="goGamePage(item)">
+      <div v-for="(item, index) in sellers" :key="index" w-120 @click="goGamePage(item)">
         <div
           flex
           justify-center
@@ -17,8 +17,8 @@
         >
           {{ item.name }}
         </div>
-        <div flex w-full bg-white rounded-b-md>
-          <img rounded-b-md w-120 h-50 object-cover :src="item._base64">
+        <div flex bg-white rounded-b-md>
+          <img rounded-b-md w-full h-50 object-cover :src="item._base64">
         </div>
       </div>
     </div>
